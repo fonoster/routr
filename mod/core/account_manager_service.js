b43b050a795d70074c21913890da575e9ec03294
Fonoster Team <fonosterteam@fonoster.com>
2017-02-07 07:20:57 -0500

    Code cleanup

--- mod/core/account_manager_service.js
+++ mod/core/account_manager_service.js
@@ -7,15 +7,11 @@ var LOG = LogManager.getLogger()
 
 // This implementation will locate gateways at config/gateways.yml
 function getGwFromConfig(ct) {
-    print ("ct -> " + ct)
     let gateways = new YamlToJsonConverter().getJson('config/gateways.yml')
     let address = ct.getOriginalRequestContact().getAddress()
     let username = address.toString().split(":")[1].split("@")[0].toString()
 
-    print ("username -> " + username)
-
     for (var gateway of gateways) {
-        print ("gateway.username -> " + gateway.username)
         if (gateway.username === username) {
 
             return gateway
@@ -27,22 +23,17 @@ function getGwFromConfig(ct) {
 }
 
 function AccountManagerService(getGateway = getGwFromConfig) {
-    print ("getGateway = " + getGateway)
-
     this.getAccountManager = function() {
         return new AccountManager() {
             getCredentials: function(challengedTransaction, realm) {
                 return new UserCredentials() {
                     getUserName: function() {
-                        print("XXXXX -> " + getGateway(challengedTransaction).username)
                         return getGateway(challengedTransaction).username
                     },
                     getPassword: function() {
-                        print("XXXXX -> " + getGateway(challengedTransaction).secret)
                         return getGateway(challengedTransaction).secret
                     },
                     getSipDomain: function() {
-                        print("XXXXX -> " + getGateway(challengedTransaction).host)
                         return getGateway(challengedTransaction).host
                     }
                 }
