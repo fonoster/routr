a1be3d458464091ec6d04d9927d9bdac3a7a3a69
Fonoster Team <fonosterteam@fonoster.com>
2017-02-14 19:20:04 -0500

    Moved default resource helper to resources.js file

---
+++ mod/core/resources.js
@@ -0,0 +1,59 @@
+/**
+ * @author Pedro Sanders
+ * @since v1
+ */
+load('mod/utils/yaml_converter.js')
+
+function getGWFromConfig(ct) {
+    const LogManager      = Java.type('org.apache.logging.log4j.LogManager')
+    const LOG = LogManager.getLogger()
+    const gateways = new YamlToJsonConverter().getJson('config/gateways.yml')
+    const gwAddress = ct.getOriginalRequestContact().getAddress()
+    const username = gwAddress.toString().split(":")[1].split("@")[0].toString()
+
+    for (var gateway of gateways) {
+        if (gateway.username === username) { return gateway }
+    }
+
+    LOG.warn ("Gateway '" + username + "' does not exist in config/gateways.yml")
+    return null
+}
+
+function getUserFromConfig(username) {
+    const agents = new YamlToJsonConverter().getJson('config/agents.yml')
+    const peers = new YamlToJsonConverter().getJson('config/peers.yml')
+
+    for (var peer of peers) {
+        if (peer.username === username) {
+            return peer
+        }
+    }
+
+    for (var agent of agents) {
+        if (agent.username === username) {
+            return agent
+        }
+    }
+
+    return null
+}
+
+function getGatewaysFromConfig() {
+    return new YamlToJsonConverter().getJson('config/gateways.yml')
+}
+
+function getDIDsFromConfig() {
+    return new YamlToJsonConverter().getJson('config/dids.yml')
+}
+
+function getAgentsFromConfig() {
+    return new YamlToJsonConverter().getJson('config/agents.yml')
+}
+
+function getPeersFromConfig() {
+    return new YamlToJsonConverter().getJson('config/peers.yml')
+}
+
+function getDomainsFromConfig() {
+    return new YamlToJsonConverter().getJson('config/domains.yml')
+}
