825a3d0b5534ac28886de8fc4f86553be7e2385d
Fonoster Team <fonosterteam@fonoster.com>
2017-04-20 17:45:47 -0400

    Update location cli-command

--- mod/ctl/cmd_location.js
+++ mod/ctl/cmd_location.js
@@ -8,8 +8,8 @@ function cmdShowLocation(id) {
     const out = Packages.java.lang.System.out
     const registry = getWithAuth('location')
 
-    out.printf('SIP devices registered\n')
-    out.printf("%-35s %-20s\n", 'ENDPOINT', 'CONTACT ADDRESS')
+    out.printf('Registered devices\n')
+    out.printf("%-35s %-20s\n", 'ADDRESS OF RECORD', 'CONTACT ADDRESS')
 
-    registry.forEach(reg => out.printf("%-35s %-20s\n", reg.endpoint, reg.contact))
+    registry.forEach(reg => out.printf("%-35s %-20s\n", reg.addressOfRecord, reg.contactAddress))
 }
