646b2a23c5b4504347b9d569dcb2147d5f1e92de
Fonoster Team <fonosterteam@fonoster.com>
2017-05-14 10:50:04 -0400

    Code cleanup

--- mod/core/config_util.js
+++ mod/core/config_util.js
@@ -2,7 +2,7 @@
  * @author Pedro Sanders
  * @since v1
  */
-load ('mod/resources/utils.js')
+load('mod/resources/utils.js')
 
 function ConfigUtil() {
     this.getConfig = () => new ResourcesUtil().getJson('config/config.yml')
