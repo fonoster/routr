dab1e9c3c2004812b39de9c58630005a512be0a7
Fonoster Team <fonosterteam@fonoster.com>
2018-01-31 20:38:24 -0500

    Added runner script

--- pack.sh
+++ pack.sh
@@ -20,6 +20,7 @@ cp sipio $FOLDER_NAME/
 cp sipioctl $FOLDER_NAME/
 cp README.md $FOLDER_NAME/
 cp LICENSE $FOLDER_NAME/
+cp run.sh $FOLDER_NAME/
 
 tar -czvf $FOLDER_NAME.tar.gz $FOLDER_NAME
 zip -r $FOLDER_NAME.zip $FOLDER_NAME
