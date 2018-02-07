78e45d67a1c8857e109622513a816acab7da0dc2
Fonoster Team <fonosterteam@fonoster.com>
2018-02-07 15:41:06 -0500

    Improve #!/usr/bin/env sh

--- run.sh
+++ run.sh
@@ -1,6 +1,12 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 
 [ -z "$EXTERN_ADDR" ] && { echo "Must define environment variable EXTERN_ADDR when running SipIO in a container"; exit 1; }
 [ -z "$LOCALNETS" ] && { export LOCALNETS=$(ip addr show eth0 | grep "inet\b" | awk '{print $2}'); }
 
-./sipio && sleep infinity
+jrunscript -Dlog4j.configurationFile=config/log4j2.xml -cp libs/app.deps.jar \
+    -e "load('libs/jvm-npm.js'); load('libs/app.bundle.js')"
+
+while sleep 3600; do :; done
+
+
+
