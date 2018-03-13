15dbb138a5646b3db5349cf5a4c915ad14b7508e
Fonoster Team <fonosterteam@fonoster.com>
2018-03-12 21:47:19 -0400

    Fixed enviroment variables

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
-[ -z "$EXTERN_ADDR" ] && { echo "Must define environment variable EXTERN_ADDR when running SipIO in a container"; exit 1; }
-[ -z "$LOCALNETS" ] && { export LOCALNETS=$(ip addr show eth0 | grep "inet\b" | awk '{print $2}'); }
+[ -z "$SIPIO_EXTERN_ADDR" ] && { echo "Must define environment variable SIPIO_EXTERN_ADDR when running SipIO in a container"; exit 1; }
+[ -z "$SIPIO_LOCALNETS" ] && { export SIPIO_LOCALNETS=$(ip addr show eth0 | grep "inet\b" | awk '{print $2}'); }
 
 jrunscript -Dlog4j.configurationFile=config/log4j2.xml -cp libs/app.deps.jar \
     -e "load('libs/jvm-npm.js'); load('libs/app.bundle.js')"
