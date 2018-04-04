2748cee0107ac7b77ee040fab3e8e7a742f5a37d
Fonoster Team <fonosterteam@fonoster.com>
2018-04-04 06:16:00 -0400

    Improved maintainability

--- mod/core/acl/acl_util.js
+++ mod/core/acl/acl_util.js
@@ -11,16 +11,9 @@ export default class AclUtil {
         this.generalAcl = generalAcl
     }
 
-    addRules(acl) {
-        if (acl.deny === undefined && acl.allow === undefined)
-            return
-
-        if (acl.deny !== undefined) {
-            acl.deny.forEach(r => { this.rules.add(new Rule('deny', r)) })
-        }
-
-        if (acl.allow !== undefined) {
-            acl.allow.forEach(r => { this.rules.add(new Rule('allow', r)) })
+    addRules(nets, action) {
+        if(nets) {
+            nets.forEach(r => { this.rules.add(new Rule(action, r))})
         }
     }
 
@@ -30,11 +23,15 @@ export default class AclUtil {
         // Is important to reset this every time
         this.rules = new java.util.ArrayList()
 
-        if (this.generalAcl !== undefined)
-            this.addRules(this.generalAcl)
+        if (this.generalAcl) {
+            this.addRules(this.generalAcl.allow, 'allow')
+            this.addRules(this.generalAcl.deny, 'deny')
+        }
 
-        if (domainObj.spec.context.accessControlList !== undefined)
-            this.addRules(domainObj.spec.context.accessControlList)
+        if (domainObj.spec.context.accessControlList) {
+            this.addRules(domainObj.spec.context.accessControlList.allow, 'allow')
+            this.addRules(domainObj.spec.context.accessControlList.deny, 'deny')
+        }
 
         return ACLHelper.mostSpecific(this.rules, calleeIp).action == 'allow'
     }
