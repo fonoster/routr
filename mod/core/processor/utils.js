6b3e417e4e8b5d8b6e1e52e39977ee7d3395ea05
Fonoster Team <fonosterteam@fonoster.com>
2018-04-08 08:05:47 -0400

    Refactored

--- mod/core/processor/utils.js
+++ mod/core/processor/utils.js
@@ -2,26 +2,28 @@
  * @author Pedro Sanders
  * @since v1
  */
+const SipFactory = Packages.javax.sip.SipFactory
+const ToHeader = Packages.javax.sip.header.ToHeader
 
- export default class ProcessorUtils {
+export default class ProcessorUtils {
 
-   /**
-    * Discover DIDs sent via a non-standard header
-    * The header must be added at config.spec.addressInfo[*]
-    * If the such header is present then overwrite the AOR
-    */
-   static getAOR (request) {
-       for (let x in this.config.spec.addressInfo) {
-           let info = this.config.spec.addressInfo[x]
-           if (!!request.getHeader(info)) {
-               let v = request.getHeader(info).getValue()
-               if (/sips?:.*@.*/.test(v) || /tel:\d+/.test(v)) {
-                   return this.addressFactory.createURI(v)
-               }
-               LOG.error('Invalid address: ' + v)
-           }
-       }
-       return request.getHeader(ToHeader.NAME).getAddress().getURI()
-   }
-
+    /**
+      * Discover DIDs sent via a non-standard header
+      * The header must be added at config.spec.addressInfo[*]
+      * If the such header is present then overwrite the AOR
+      */
+    static getAOR (request, addressInfo = []) {
+      for (const x in addressInfo) {
+          let info = addressInfo[x]
+          if (request.getHeader(info) != undefined) {
+              let v = request.getHeader(info).getValue()
+              if (/sips?:.*@.*/.test(v) || /tel:\d+/.test(v)) {
+                  const addressFactory = SipFactory.getInstance().createAddressFactory()
+                  return addressFactory.createURI(v)
+              }
+              LOG.error('Invalid address: ' + v)
+          }
+      }
+      return request.getHeader(ToHeader.NAME).getAddress().getURI()
+    }
 }
