d0712a71d090591f74415fac9bdfc2947feeb91c
Vitaly Takmazov <vitalyster@gmail.com>
2018-06-07 14:55:50 +0300

    sipio.bat: use JVM from JAVA_HOME if available with fallback to bundled copy

--- sipio.bat
+++ sipio.bat
@@ -1,5 +1,11 @@
 @echo off
+setlocal
 
-.\jre\bin\jrunscript.exe -Dlog4j.configurationFile=config/log4j2.xml -cp libs\app.deps.jar -e "var home = Packages.java.lang.System.getProperty('user.dir');load(home + '\\libs\\jvm-npm.js');load(home + '\\libs\\app.bundle.js')"
+if defined JAVA_HOME goto run
+set JAVA_HOME=jre
 
+:run
+"%JAVA_HOME%\bin\jrunscript.exe" -Dlog4j.configurationFile=config/log4j2.xml -cp libs\app.deps.jar -e "var home = Packages.java.lang.System.getProperty('user.dir');load(home + '\\libs\\jvm-npm.js');load(home + '\\libs\\app.bundle.js')"
+
+endlocal
 @echo on
