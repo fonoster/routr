aaad454c501b053d14084475fdb5f57484ed16be
Fonoster Team <fonosterteam@fonoster.com>
2018-07-08 12:06:44 -0400

    Updated site link

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -22,8 +22,8 @@ const users = [
 
 const siteConfig = {
   tagline: 'Next-generation Sip Server',
-  url: 'https://fonoster.github.com' /* your website url */,
-  baseUrl: '/routr/' /* base url for your project */,
+  url: 'https://routr.io' /* your website url */,
+  baseUrl: '/' /* base url for your project */,
   // For github.io type URLs, you would set the url and baseUrl like:
   //url: 'https://fonoster.github.io/routr',
   //baseUrl: '/',
