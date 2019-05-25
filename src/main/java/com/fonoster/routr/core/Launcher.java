d720f11f9d1ffae76eab42427e64f9b7298ace04
Fonoster Team <fonosterteam@fonoster.com>
2019-05-25 16:39:12 -0400

    Cleanup

--- src/main/java/com/fonoster/routr/core/Launcher.java
+++ src/main/java/com/fonoster/routr/core/Launcher.java
@@ -12,16 +12,16 @@ import javax.script.ScriptException;
 public class Launcher {
     private static String launchScript = "var System = Java.type('java.lang.System');"
       + "load(System.getProperty('user.dir') + '/libs/app.bundle.js')";
-    private static Timer timer = new Timer();
 
     static public void main(String... args) {
         new Launcher().launch();
     }
 
+    // TODO: Check Java version and show warning if version >= 11
     public void launch() {
         String engine = System.getenv("ROUTR_JS_ENGINE");
 
-        if (engine != null && engine.equals("graaljs")) {
+        if (engine != null && engine.equals("graal.js")) {
           launchWithGraalJS();
         } else if (engine != null && engine.equals("nashorn")) {
           launchWithNashorn();
@@ -33,7 +33,7 @@ public class Launcher {
     public void launchWithNashorn() {
         try {
             ScriptEngine engine = new ScriptEngineManager().getEngineByName("nashorn");
-            engine.put("timer", timer);
+            engine.put("timer", new Timer());
             engine.eval(launchScript);
         } catch(ScriptException ex) {
             System.out.println("Unable to run with 'nashorn' engine: " + ex.getMessage());
@@ -48,7 +48,7 @@ public class Launcher {
           .allowIO(true)
           .allowAllAccess(true).build();
 
-          polyglot.getBindings("js").putMember("timer", timer);
+          polyglot.getBindings("js").putMember("timer", new Timer());
           polyglot.eval("js", launchScript);
     }
 }
