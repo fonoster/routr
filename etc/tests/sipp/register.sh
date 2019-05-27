c6b20930294c53fac77aca51ef044a7dfd7138bc
Fonoster Team <fonosterteam@fonoster.com>
2019-05-27 17:19:36 -0400

    Changed test parameters

--- etc/tests/sipp/register.sh
+++ etc/tests/sipp/register.sh
@@ -4,6 +4,7 @@ SIPP_PORT=5080
 ROUTR_HOST=192.168.1.127
 SCENARIO_FILE=uac_register.xml
 REGISTER_FILE=register.csv
+REPORT_FILE=report.trace
 
 docker pull ctaloi/sipp
 
@@ -11,9 +12,12 @@ docker run -it -p $SIPP_PORT:$SIPP_PORT/udp \
     -v $PWD:/sipp \
     ctaloi/sipp $ROUTR_HOST \
     -t t1 \
+    -trace_err \
+    -trace_stat \
     -p $SIPP_PORT \
     -sf $SCENARIO_FILE \
     -inf $REGISTER_FILE \
-    -r 5000 \
-    -m 200000 \
-    -l 100000
+    -r 1000 \
+    -m 100000 \
+    -l 10000 \
+    -rate_increase 1000 -fd 5s
