11e12c8607a5832f285ff75346b9983c975ad594
Fonoster Team <fonosterteam@fonoster.com>
2019-06-19 15:23:20 -0400

    Added jvm profiler

--- routr.bat
+++ routr.bat
@@ -10,6 +10,8 @@ exit 1
 
 :run
 cd %BASE_DIR%
+REM Uncomment to activate jvm profiler. You must use a full JVM(not the custom one) to profile CPU usage
+REM set ROUTR_JAVA_OPTS=-javaagent:libs/jvm-profiler-master-SNAPSHOT.jar=reporter=com.uber.profiling.reporters.FileOutputReporter,outputDir=out,tag=routr,metricInterval=5000
 "%JAVA_HOME%\bin\java" %ROUTR_JAVA_OPTS% -Dlog4j.configurationFile=config\log4j2.xml -classpath libs\* com.fonoster.routr.core.Launcher
 
 endlocal
