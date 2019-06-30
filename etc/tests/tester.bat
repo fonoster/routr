aabac809e331c694d21e3c870352ec45acb5a820
Pedro Sanders <sanderspedro@gmail.com>
2019-06-30 18:57:58 -0400

    Improved test scripts

--- etc/tests/tester.bat
+++ etc/tests/tester.bat
@@ -1,3 +1,9 @@
 set main_js=etc\tests\test_helper.js
 
+if defined JAVA_HOME goto run
+echo 'Could not find a runtime environment. Please setup environment variable JAVA_HOME'
+exit 1
+
+run:
+
 "%JAVA_HOME%\bin\jjs" -Dlog4j.configurationFile=./config/log4j2.xml -cp libs/app.deps.jar -doe=true -ot=true -strict=false -scripting=true %main_js% -- %*
