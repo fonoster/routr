9a75eb18f61db6b8697c0924ce5a718a1aadb50c
Pedro Sanders <sanderspedro@gmail.com>
2019-08-26 18:52:48 -0400

    Work on test environment

--- mod/location/test2.js
+++ mod/location/test2.js
@@ -1,19 +1,8 @@
 
 describe('Location Tests v2', () => {
 
-    const Registry = require('../registry/registry.js')
-    const GatewaysAPI = require('../data_api/gateways_api.js')
-    const InetAddress = Java.type('java.net.InetAddress')
-    const SipFactory = Java.type('javax.sip.SipFactory')
-    const addressFactory = SipFactory.getInstance().createAddressFactory()
-    const dataAPIs = {
-        GatewaysAPI: new GatewaysAPI()
-    }
 
     it('No shit', function(done) {
-        const registry = new Registry(null, dataAPIs)
-        registry.storeRegistry(addressFactory.createSipURI('29121', 'sanjose2.voip.ms'), 200)
-        assertTrue(registry.listAsJSON().length === 1)
         done()
     })
 
