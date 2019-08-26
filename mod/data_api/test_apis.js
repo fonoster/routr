2fc9359cde68decf7e7ed5915cdeb4b19de3a691
Pedro Sanders <sanderspedro@gmail.com>
2019-08-26 18:45:44 -0400

    Testing mocha,instabull and travis

--- mod/data_api/test_apis.js
+++ mod/data_api/test_apis.js
@@ -28,3 +28,24 @@ testGroup.deep_search = function() {
 }
 
 module.exports.testGroup = testGroup
+
+describe('Data API v2', () => {
+
+    it('No shit', function(done) {
+        const agents = []
+        agents.push(TestUtils.buildAgent('John Doe', ['sip.local'], '1001'))
+        agents.push(TestUtils.buildAgent('Jane Doe', ['sip.local'], '1002'))
+        agents.push(TestUtils.buildAgent('Jake Doe', ['sip.local'], '1003'))
+
+        let response = {
+            status: 200,
+            result: agents
+        }
+
+        response = DSUtils.deepSearch(response, 'spec.credentials.username', '1001')
+        const agent = response.result
+        assertTrue(agent.metadata.name === 'John Doe')
+        done()
+    })
+
+})
