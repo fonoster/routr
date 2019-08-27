e5e8969cfb9cc2e6b6afc511a54955b7b56c2ac3
Pedro Sanders <sanderspedro@gmail.com>
2019-08-26 22:18:24 -0400

    Changed to use js replace instead of replaceAll from java

--- mod/core/config_util.js
+++ mod/core/config_util.js
@@ -198,7 +198,7 @@ function getSalt() {
 
     if (f.exists() && !f.isDirectory()) return FilesUtil.readFile(pathToSalt)
 
-    const genSalt = UUID.randomUUID().toString().replaceAll("-", "")
+    const genSalt = UUID.randomUUID().toString().replace(/\-/g,"")
     FilesUtil.writeFile(pathToSalt, genSalt)
 
     return genSalt
