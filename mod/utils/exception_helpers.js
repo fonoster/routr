c2551ea33b3c9e155f73e76cc0f056ce98c3bce8
Pedro Sanders <sanderspedro@gmail.com>
2019-09-07 16:57:17 -0400

    Refactored connection handler

---
+++ mod/utils/exception_helpers.js
@@ -0,0 +1,16 @@
+/**
+ * @author Pedro Sanders
+ * @since v1
+ */
+module.exports.connectionException = (e, host) => {
+    const LogManager = Java.type('org.apache.logging.log4j.LogManager')
+    const LOG = LogManager.getLogger()
+    if (e instanceof Java.type('javax.sip.TransactionUnavailableException') ||
+        e instanceof Java.type('java.net.ConnectException')                 ||
+        e instanceof Java.type('java.net.NoRouteToHostException')           ||
+        e.toString().includes('Could not create a message channel for')) {
+        LOG.warn(`Unable to connect to host -> \`${host}\`.(Verify your network status)`)
+    } else {
+        LOG.error(e)
+    }
+}
