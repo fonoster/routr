35af35cecaed2e166a0a857a727e2d778acb1b0c
Pedro Sanders <sanderspedro@gmail.com>
2019-09-07 07:19:57 -0400

    Added DS selector

---
+++ mod/data_api/ds_selector.js
@@ -0,0 +1,25 @@
+const FilesDataSource = require('@routr/data_api/files_datasource')
+const RedisDataSource = require('@routr/data_api/redis_datasource')
+const config = require('@routr/core/config_util')()
+
+class DSSelector {
+
+    constructor() {
+        if (config.spec.dataSource.provider === 'files_data_provider') {
+            this.dataSource = new FilesDataSource()
+        } else if (config.spec.dataSource.provider === 'redis_data_provider') {
+            this.dataSource = new RedisDataSource()
+        } else {
+            throw 'Invalid data source'
+        }
+    }
+
+    getDS() {
+        return this.dataSource
+    }
+
+}
+
+const instace = new DSSelector()
+Object.freeze(instace)
+module.exports = instace
