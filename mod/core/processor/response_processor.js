8cbd60cf3b7e6308bb04c1804669e8239a250498
Pedro Sanders <sanderspedro@gmail.com>
2019-09-07 11:40:02 -0400

    Cleanup

--- mod/core/processor/response_processor.js
+++ mod/core/processor/response_processor.js
@@ -2,107 +2,43 @@
  * @author Pedro Sanders
  * @since v1
  */
-const AccountManagerService = require('@routr/core/account_manager_service')
-const RegistrarUtils = require('@routr/registrar/utils')
-
-const SipFactory = Java.type('javax.sip.SipFactory')
-const FromHeader = Java.type('javax.sip.header.FromHeader')
+const {
+    isStackJob,
+    isTransactional,
+    mustAuthenticate,
+    handleAuthChallenge
+} = require('@routr/core/processor/response_utils')
 const ViaHeader = Java.type('javax.sip.header.ViaHeader')
-const ContactHeader = Java.type('javax.sip.header.ContactHeader')
-const CSeqHeader = Java.type('javax.sip.header.CSeqHeader')
-const ExpiresHeader = Java.type('javax.sip.header.ExpiresHeader')
-const Request = Java.type('javax.sip.message.Request')
-const Response = Java.type('javax.sip.message.Response')
 const LogManager = Java.type('org.apache.logging.log4j.LogManager')
 const LOG = LogManager.getLogger()
 
 class ResponseProcessor {
 
-    constructor(sipProvider, dataAPIs, contextStorage, registry) {
+    constructor(sipProvider, dataAPIs, contextStorage) {
         this.sipProvider = sipProvider
-        this.registry = registry
         this.contextStorage = contextStorage
-        this.headerFactory = SipFactory.getInstance().createHeaderFactory()
-        this.accountManagerService = new AccountManagerService(dataAPIs)
     }
 
     process(event) {
-        const response = event.getResponse()
-
-        // The stack takes care of this cases
-        if (ResponseProcessor.isStackJob(response)) {
+        if (isStackJob(event.getResponse())) {
             return
         }
-
-        // This response is owned by server
-        if (ResponseProcessor.isRegisterOk(response)) {
-            return ResponseProcessor.isBehindNat(response) ?
-              this.reRegister(event):
-              this.storeInRegistry(response)
-        } else if (ResponseProcessor.isRegisterNok(response)) {
-            this.removeFromRegistry(response)
+        // If it is not transactional and athentication is required it means
+        // that the REGISTER request was originated by another sipStack
+        if(mustAuthenticate(event.getResponse()) && isTransactional(event)) {
+            const gwRef = event.getClientTransaction().getRequest()
+              .getHeader('X-Gateway-Ref').value
+            const r = gatewaysAPI.getGateway(gwRef)
+            handleAuthChallenge(his.sipProvider.getSipStack(), event, r.result)
+            return
         }
-
-        ResponseProcessor.mustAuthenticate(response) ? this.handleAuthChallenge(event) :
-            this.sendResponse(event)
-    }
-
-    storeInRegistry(response) {
-        const fromURI = response.getHeader(FromHeader.NAME).getAddress().getURI()
-        this.registry.storeRegistry(fromURI, RegistrarUtils.getExpires(response))
-    }
-
-    removeFromRegistry(response) {
-        const fromURI = response.getHeader(FromHeader.NAME).getAddress().getURI()
-        this.registry.removeRegistry(fromURI.toString())
-    }
-
-    handleAuthChallenge(event) {
-        try {
-            const authHelper = this.sipProvider
-                .getSipStack()
-                .getAuthenticationHelper(this.accountManagerService
-                    .getAccountManager(), this.headerFactory)
-            // Setting looseRouting to false will cause https://github.com/fonoster/routr/issues/18
-            authHelper.handleChallenge(
-                event.getResponse(), event.getClientTransaction(),
-                event.getSource(), 5, true).sendRequest()
-        } catch(e) {
-            if (e instanceof Java.type('javax.sip.TransactionUnavailableException')) {
-                LOG.error('Could not resolve next hop or listening point unavailable!')
-            } else {
-                LOG.error(e)
-            }
-        }
-    }
-
-    reRegister(event) {
-        const response = event.getResponse()
-        const clientTransaction = event.getClientTransaction()
-        const viaHeader = response.getHeader(ViaHeader.NAME)
-        const expires = RegistrarUtils.getExpires(response)
-
-        LOG.debug('Routr is behind a NAT. Re-registering using Received and RPort')
-
-        try {
-            const fromURI = response.getHeader(FromHeader.NAME).getAddress().getURI()
-            const gwRef = clientTransaction.getRequest().getHeader('X-Gateway-Ref').value
-            this.registry.requestChallenge(fromURI.getUser(),
-                gwRef,
-                fromURI.getHost(),
-                viaHeader.getTransport().toLowerCase(),
-                viaHeader.getReceived(),
-                viaHeader.getRPort(),
-                expires)
-        } catch (e) {
-            LOG.error(e)
-        }
+        this.sendResponse(event)
     }
 
     sendResponse(event) {
         const responseOut = event.getResponse().clone()
         responseOut.removeFirst(ViaHeader.NAME)
-        if (ResponseProcessor.isTransactional(event) === true) {
+        if (isTransactional(event)) {
             const context = this.contextStorage.findContext(event.getClientTransaction().getBranchId())
 
             if (context && context.serverTransaction) {
@@ -112,69 +48,10 @@ class ResponseProcessor {
             }
         } else if (responseOut.getHeader(ViaHeader.NAME) !== null) {
             // Could be a BYE due to Record-Route
-            // There is no more Via headers; the response was intended for the proxy.
             this.sipProvider.sendResponse(responseOut)
         }
 
         LOG.debug(responseOut)
-    }
-
-    static isOk(response) {
-        return response.getStatusCode() === Response.OK
-    }
-
-    static mustAuthenticate(response) {
-        return response.getStatusCode() === Response.PROXY_AUTHENTICATION_REQUIRED ||
-            response.getStatusCode() === Response.UNAUTHORIZED
-    }
-
-    static isInviteOrMessage(response) {
-        return response.getHeader(CSeqHeader.NAME).getMethod() === Request.INVITE ||
-            Request.MESSAGE
-    }
-
-    static isInviteOrMessageOk(response) {
-        return ResponseProcessor.isInviteOrMessage(response) && ResponseProcessor.isOk(response)
-    }
-
-    static isInviteOrMessageNok(response) {
-        return ResponseProcessor.isInviteOrMessage(response) && !ResponseProcessor.isOk(response)
-    }
-
-    static isStackJob(response) {
-        return response.getStatusCode() === Response.TRYING ||
-            response.getStatusCode() === Response.REQUEST_TERMINATED ||
-            response.getHeader(CSeqHeader.NAME).getMethod() === Request.CANCEL
-    }
-
-    static isBehindNat(response) {
-        const viaHeader = response.getHeader(ViaHeader.NAME)
-        //const contactHeader = response.getHeader(ContactHeader.NAME)
-        //const host = contactHeader.getAddress().getHost()
-        //const port = contactHeader.getAddress().getPort()
-        const host = viaHeader.getHost()
-        const port = viaHeader.getPort()
-        const received = viaHeader.getReceived()
-        const rPort = viaHeader.getRPort()
-
-        return (!!received && !host.equals(received)) || port !== rPort
-    }
-
-    static isRegister(response) {
-        const cseq = response.getHeader(CSeqHeader.NAME)
-        return cseq.getMethod() === Request.REGISTER
-    }
-
-    static isRegisterOk(response) {
-        return ResponseProcessor.isRegister(response) && ResponseProcessor.isOk(response)
-    }
-
-    static isRegisterNok(response) {
-        return !ResponseProcessor.isOk(response) && ResponseProcessor.isRegister(response)
-    }
-
-    static isTransactional(event) {
-        return ResponseProcessor.isInviteOrMessage(event.getResponse()) && event.getClientTransaction() !== null
     }
 
 }
--- mod/core/processor/response_utils.js
+++ mod/core/processor/response_utils.js
@@ -6,6 +6,10 @@ const CSeqHeader = Java.type('javax.sip.header.CSeqHeader')
 const ViaHeader = Java.type('javax.sip.header.ViaHeader')
 const Request = Java.type('javax.sip.message.Request')
 const Response = Java.type('javax.sip.message.Response')
+const AccountManager = Java.type('gov.nist.javax.sip.clientauthutils.AccountManager')
+const UserCredentials = Java.type('gov.nist.javax.sip.clientauthutils.UserCredentials')
+const SipFactory = Java.type('javax.sip.SipFactory')
+const headerFactory = SipFactory.getInstance().createHeaderFactory()
 
 const hasCodes = (r, c) => c.filter(code => r.getStatusCode() === code).length > 0
 const isMethod = (r, m) => m.filter(method => r.getHeader(CSeqHeader.NAME).getMethod() === method).length > 0
@@ -22,6 +26,33 @@ const isBehindNat = r =>  {
     const v = r.getHeader(ViaHeader.NAME)
     return !v.getHost().equals(v.getReceived()) || v.getPort() !== v.getRPort()
 }
+const getAccountManager = gateway => {
+    const buildAddr = (h, p) => `${h}${p? ':' + p: ''}`
+    return new AccountManager({
+        getCredentials: () => {
+            return new UserCredentials({
+                getUserName: () => gateway.spec.credentials.username,
+                getPassword: () => gateway.spec.credentials.secret,
+                getSipDomain: () => buildAddr(gateway.spec.host, gateway.spec.port)
+            })
+        }
+    })
+}
+const handleAuthChallenge = (sipStack, e, gateway) => {
+    try {
+        const accountManager = getAccountManager(gateway)
+        const authHelper = sipStack.getAuthenticationHelper(accountManager, headerFactory)
+        // Setting looseRouting to false will cause https://github.com/fonoster/routr/issues/18
+        authHelper.handleChallenge(e.getResponse(), e.getClientTransaction(), e.getSource(), 5, true).sendRequest()
+    } catch(e) {
+        if (e instanceof Java.type('javax.sip.TransactionUnavailableException')) {
+            LOG.error('Could not resolve next hop or listening point unavailable!')
+        } else {
+            LOG.error(e)
+        }
+        e.printStackTrace()
+    }
+}
 
 module.exports.hasCodes = hasCodes
 module.exports.isMethod = isMethod
@@ -33,3 +64,5 @@ module.exports.isRegister = isRegister
 module.exports.isRegisterOk = isRegisterOk
 module.exports.isRegisterNok = isRegisterNok
 module.exports.isBehindNat = isBehindNat
+module.exports.isBehindNat = isBehindNat
+module.exports.handleAuthChallenge = handleAuthChallenge
