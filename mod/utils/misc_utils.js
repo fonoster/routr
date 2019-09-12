25494cf6d765c961cf0615050cabfc3f3b24d367
Pedro Sanders <sanderspedro@gmail.com>
2019-09-12 17:47:24 -0400

    Cleanup

--- mod/utils/misc_utils.js
+++ mod/utils/misc_utils.js
@@ -5,9 +5,7 @@
  * Miscellaneous utilities
  */
 module.exports.equalsIgnoreCase = (a, b) => a.toLowerCase() === b.toLowerCase()
-
 module.exports.buildAddr = (h, p) => `${h}${p? ':' + p: ''}`
-
 module.exports.protocolTransport = (config, proto) => {
     try {
         const transport = config.spec.transport.filter(trans => trans.protocol === proto)[0]
@@ -17,7 +15,6 @@ module.exports.protocolTransport = (config, proto) => {
         throw `Transport \'${proto}\' not found in configs => .spec.transport.[*]`
     }
 }
-
 // Returns the address for the neareast interface to a targeted host
 module.exports.nearestInterface = (h, p, h1, p1) => {
     const host = h1? h1 : h
