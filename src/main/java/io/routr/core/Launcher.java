75b98145eef7ff6bd2a99645f0236c4df797b3a4
Pedro Sanders <sanderspedro@gmail.com>
2019-09-14 12:41:18 -0400

    Improves multi-engine suppport

--- src/main/java/io/routr/core/Launcher.java
+++ src/main/java/io/routr/core/Launcher.java
@@ -9,8 +9,15 @@ import java.util.Timer;
 import javax.script.*;
 
 public class Launcher {
+  // Copied from https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/includes#Polyfill
+    public static final String NASHORN_POLYFILL_STRING_PROTOTYPE_INCLUDES = "if (!String.prototype.includes) { Object.defineProperty(String.prototype, 'includes', { value: function(search, start) { if (typeof start !== 'number') { start = 0 } if (start + search.length > this.length) { return false } else { return this.indexOf(search, start) !== -1 } } }) }";
+    // Copied from https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/includes#Polyfill
+    public static final String NASHORN_POLYFILL_ARRAY_PROTOTYPE_INCLUDES  = "if (!Array.prototype.includes) { Object.defineProperty(Array.prototype, 'includes', { value: function(valueToFind, fromIndex) { if (this == null) { throw new TypeError('\"this\" is null or not defined'); } var o = Object(this); var len = o.length >>> 0; if (len === 0) { return false; } var n = fromIndex | 0; var k = Math.max(n >= 0 ? n : len - Math.abs(n), 0); function sameValueZero(x, y) { return x === y || (typeof x === 'number' && typeof y === 'number' && isNaN(x) && isNaN(y)); } while (k < len) { if (sameValueZero(o[k], valueToFind)) { return true; } k++; } return false; } }); }";
+
     private static String baseScript = String.join(
         System.getProperty("line.separator"),
+        NASHORN_POLYFILL_STRING_PROTOTYPE_INCLUDES,
+        NASHORN_POLYFILL_ARRAY_PROTOTYPE_INCLUDES,
         "var System = Java.type('java.lang.System')",
         "load(System.getProperty('user.dir') + '/libs/jvm-npm.js')"
     );
@@ -53,7 +60,7 @@ public class Launcher {
         }
 
         JSEngine engine = JSEngine.get(System.getenv("ROUTR_JS_ENGINE"));
-  
+
         ScriptEngine mainCtx = createJSContext(engine);
         ScriptEngine registryCtx = createJSContext(engine);
         ScriptEngine restCtx = createJSContext(engine);
