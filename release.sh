ec25cf726ca2208d17a5906838a99a756f6f2596
Pedro Sanders <sanderspedro@gmail.com>
2019-09-28 12:32:39 -0400

    Work on release workflow

--- release.sh
+++ release.sh
@@ -1,9 +1,9 @@
 
 {
   git tag -d 1.0.0-rc4.pre
-  git push origin :refs/tags/1.0.0-rc4.pre
+#  git push origin :refs/tags/1.0.0-rc4.pre
 } 2> /dev/null
 
 git tag 1.0.0-rc4.pre
 git push
-git push --tags
+#git push --tags
