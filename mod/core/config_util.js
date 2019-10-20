52f76181d760c03f1f5482c538fd7fb54ba0b824
Pedro Sanders <sanderspedro@gmail.com>
2019-10-20 16:38:36 -0400

    Minor fix

--- mod/core/config_util.js
+++ mod/core/config_util.js
@@ -190,7 +190,10 @@ function getConfigFromFile() {
 function getSalt() {
     if (System.getenv('ROUTR_SALT') !== null) return System.getenv('ROUTR_SALT')
 
-    const pathToSalt = `${System.getProperty('user.dir')}/.routr.salt`
+    const pathToSalt = System.getenv('ROUTR_SALT_FILE') !== null ?
+        System.getenv('ROUTR_SALT_FILE') :
+        `${System.getProperty('user.dir')}/.routr.salt`
+
     const f = new File(pathToSalt)
 
     if (f.exists() && !f.isDirectory()) return FilesUtil.readFile(pathToSalt)
