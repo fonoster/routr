6a855133ec2fc7057826b7dc4729156da9d1b3b8
Pedro Sanders <sanderspedro@gmail.com>
2020-03-26 09:05:42 -0400

    Fixed formating

--- mod/core/extern_info.js
+++ mod/core/extern_info.js
@@ -7,7 +7,7 @@ Java.type('org.apache.logging.log4j.LogManager')
 const LOG = LogManager.getLogger()
 const ANSI_GREEN = '\u001B[32m'
 const ANSI_YELLOW = '\u001B[33m'
-const ANSI_RESET =      '\u001B[0m'
+const ANSI_RESET =       '\u001B[0m'
 
 module.exports = config => {
   if (config.spec.externAddr) {
