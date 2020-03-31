d1576ef6c776dce893dff003869bd90267b61570
Pedro Sanders <sanderspedro@gmail.com>
2020-03-31 14:00:13 -0400

    Added unit test

---
+++ mod/data_api/domains.unit.test.js
@@ -0,0 +1,68 @@
+/**
+ * @author Pedro Sanders
+ * @since v1
+ *
+ * Unit Test for the "Domains API"
+ */
+const APIBase = require('@routr/data_api/api_base')
+const DomainsAPI = require('@routr/data_api/domains_api')
+const TestUtils = require('@routr/data_api/test_utils')
+const chai = require('chai')
+const sinon = require('sinon')
+const sinonChai = require('sinon-chai')
+chai.use(sinonChai)
+const expect = chai.expect
+var sandbox = sinon.createSandbox()
+
+describe('@routr/data_api/domains', () => {
+  context('createFromJson', () => {
+    let domains, jsonObj
+
+    beforeEach(() => {
+      domains = new DomainsAPI()
+      jsonObj = TestUtils.buildDomain('SIP Local', 'sip.local', '001')
+    })
+
+    afterEach(() => {
+      sandbox.restore()
+    })
+
+    it('should be a valid resource', () => {
+      delete jsonObj.kind
+      const result = domains.createFromJSON(jsonObj)
+      expect(result).to.be.deep.equal({
+        data:
+          'Not a valid entity. `kind` must be: User, Agent, Peer, Domain, Gateway, Number',
+        status: 422,
+        message: 'Unprocesssable entity'
+      })
+    })
+
+    it('should have an existing number or no number at all', () => {
+      const doesNumberExist = sinon
+        .stub(domains, 'doesNumberExist')
+        .returns(false)
+      const result = domains.createFromJSON(jsonObj)
+      expect(doesNumberExist).to.has.been.calledOnce
+      expect(result).to.be.deep.equal({
+        status: 409,
+        message: 'Found one or more unfulfilled dependencies'
+      })
+    })
+
+    it('should not allow duplicates', () => {
+      const doesNumberExist = sinon
+        .stub(domains, 'doesNumberExist')
+        .returns(true)
+      const domainExist = sinon.stub(domains, 'domainExist').returns(true)
+      const result = domains.createFromJSON(jsonObj)
+
+      expect(doesNumberExist).to.has.been.calledOnce
+      expect(domainExist).to.has.been.calledOnce
+      expect(result).to.be.deep.equal({
+        status: 409,
+        message: 'Entity already exist'
+      })
+    })
+  })
+})
---
+++ mod/data_api/utils.unit.test.js
@@ -0,0 +1,95 @@
+/**
+ * @author Pedro Sanders
+ * @since v1
+ *
+ * Unit Test for the "Domains API"
+ */
+const DSUtils = require('@routr/data_api/utils')
+const TestUtils = require('@routr/data_api/test_utils')
+const chai = require('chai')
+const sinon = require('sinon')
+const sinonChai = require('sinon-chai')
+chai.use(sinonChai)
+const expect = chai.expect
+var sandbox = sinon.createSandbox()
+
+describe('@routr/data_api/utils', () => {
+  context('validateEntity', () => {
+    let domains, jsonObj
+
+    beforeEach(() => {
+      jsonObj = TestUtils.buildGateway('SIP Local', 'sip.local', '001')
+    })
+
+    afterEach(() => {
+      sandbox.restore()
+    })
+
+    it('should be a valid kind', () => {
+      delete jsonObj.kind
+      const result = DSUtils.validateEntity(jsonObj)
+      expect(result).to.be.a('array')
+      expect(result[0]).to.be.equal(
+        'Not a valid entity. `kind` must be: User, Agent, Peer, Domain, Gateway, Number'
+      )
+    })
+
+    it('should be a valid resource', () => {
+      const validateObj = sinon
+        .stub(DSUtils, 'validateObj')
+        .returns(['some error'])
+      const result = DSUtils.validateEntity(jsonObj)
+
+      expect(validateObj).to.has.been.calledOnce
+      expect(result).to.be.a('array')
+      validateObj.restore()
+    })
+
+    it('should not allow re-write of read-only fields', () => {
+      const jObjUpdated = TestUtils.buildGateway(
+        'SIP Local',
+        'sip.local',
+        '001'
+      )
+      jObjUpdated.kind = 'v1'
+      const result = DSUtils.validateEntity(jsonObj, jObjUpdated, 'write')
+      expect(result).to.be.a('array')
+      expect(result[0]).to.be.equal(
+        '$[0].kind: is a readonly field, it cannot be changed'
+      )
+    })
+
+    it('should have a valid transport', () => {
+      jsonObj.spec.transport = 'tcpx'
+      const result = DSUtils.validateEntity(jsonObj)
+      expect(result)
+        .to.be.a('array')
+        .lengthOf(1)
+      expect(result[0]).to.be.equal(
+        '$[0].spec.transport: does not have a value in the enumeration [tcp, udp]'
+      )
+    })
+
+    it('should have a name with three or more characters', () => {
+      jsonObj.metadata.name = 'ab'
+      const result = DSUtils.validateEntity(jsonObj)
+      expect(result)
+        .to.be.a('array')
+        .lengthOf(1)
+      expect(result[0]).to.be.equal(
+        '$[0].metadata.name: must be at least 3 characters long'
+      )
+    })
+
+    it('should have a reference with three but no more than 25', () => {
+      jsonObj.metadata.ref = 'sdsdkskskdksksdksksdkdksdksdkskdksdk'
+      const result = DSUtils.validateEntity(jsonObj)
+      expect(result)
+        .to.be.a('array')
+        .lengthOf(1)
+      expect(result[0]).to.be.equal(
+        '$[0].metadata.ref: may only be 24 characters long'
+      )
+    })
+  })
+})
