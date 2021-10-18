62e606dcdeab769289a943d8ad6ce9496047ef12
Pedro Sanders <sanderspedro@gmail.com>
2021-10-18 11:45:20 -0400

    Refactored

--- .snapcraft/resources/start.sh
+++ .snapcraft/resources/start.sh
@@ -3,17 +3,17 @@
 set -e
 
 function start {
-		externAddr="$(snapctl get routr-extern-addr)"
-		if [ "$externAddr" ]; then
-				export ROUTR_EXTERN_ADDR=$externAddr
-		fi
+  externAddr="$(snapctl get routr-extern-addr)"
+  if [ "$externAddr" ]; then
+    export EXTERN_ADDR=$externAddr
+  fi
 
-		localnets="$(snapctl get routr-localnets)"
-		if [ "$localnets" ]; then
-				export ROUTR_LOCALNETS=$localnets
-		fi
+  localnets="$(snapctl get routr-localnets)"
+  if [ "$localnets" ]; then
+    export LOCALNETS=$localnets
+  fi
 
-		$SNAP/routr
+  $SNAP/routr
 }
 
 start
