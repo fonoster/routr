63e6c073884d3e572224fcdb8f4adc859ac9de74
Pedro Sanders <psanders@fonoster.com>
2022-03-05 17:46:05 -0500

    Refactored

--- mods/edgeport/edgeport.sh
+++ mods/edgeport/edgeport.sh
@@ -5,4 +5,4 @@
 
 export JAVA_OPTS="-Dpolyglot.js.experimental-foreign-object-prototype=true -Dpolyglot.js.nashorn-compat=true -Dnashorn.args=--language=es6 -Xmn256m -Xmx7000M -Xms1024M -XX:CMSInitiatingOccupancyFraction=80 -Dsun.rmi.dgc.client.gcInterval=3600000 -Djava.net.preferIPv4Stack=true"
 
-$JAVA_HOME/bin/java $JAVA_OPTS -classpath "libs/*" io.routr.Launcher
+$JAVA_HOME/bin/java $JAVA_OPTS -classpath "mods/edgeport/build/libs:mods/edgeport/libs/*" io.routr.Launcher
