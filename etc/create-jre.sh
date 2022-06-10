380312aeda7829c13a3b2d514841a609d64b669b
Pedro Sanders <psanders@fonoster.com>
2022-06-10 14:02:45 -0400

    Added script to create custom jre in darwin and linux

---
+++ etc/create-jre.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+[ -z "$JAVA_HOME" ] && { echo "Please setup the JAVA_HOME environment variable"; exit 1; }
+
+echo "JAVA_HOME=$JAVA_HOME"
+
+rm -rf jre
+
+$JAVA_HOME/bin/jlink -c --no-man-pages --no-header-files -G \
+  --module-path ${JAVA_HOME}/jmods/ \
+  --add-modules \
+  java.base,java.management,java.naming,java.sql,jdk.crypto.cryptoki,jdk.scripting.nashorn,jdk.management.agent,jdk.unsupported,jdk.unsupported.desktop \
+  --output jre
