3da88d43421ab5f52127c60637b82b0b803098f6
Pedro Sanders <psanders@fonoster.com>
2022-10-16 10:49:59 -0400

    feat: added registry service to runner

--- .scripts/runner.ts
+++ .scripts/runner.ts
@@ -1,12 +1,14 @@
 #!/usr/bin/env node
-import ConnectProcessor from "../mods/connect/src/service"
-import SimpleAuthMiddleware from "../mods/simpleauth/src/service"
-import SimpleDataService from "../mods/simpledata/src/service"
-import MessageDispatcher from "../mods/dispatcher/src/service"
-import LocationService from "../mods/location/src/service"
+import connectProcessor from "../mods/connect/src/service"
+import simpleAuthMiddleware from "../mods/simpleauth/src/service"
+import simpleDataService from "../mods/simpledata/src/service"
+import messageDispatcher from "../mods/dispatcher/src/service"
+import locationService from "../mods/location/src/service"
+import registryService from "../mods/registry/src/service"
 import loadResources from '../mods/simpledata/src/utils'
 import {getConfig as getDispatcherConfig} from "../mods/dispatcher/src/config/get_config"
 import {getConfig as getLocationConfig} from "../mods/location/src/config/get_config"
+import {getConfig as getRegistryConfig} from "../mods/registry/src/config/get_config"
 import {spawn} from "child_process"
 import {Helper as H} from "../mods/common"
 import {Resource} from '../mods/common/src/connect/types'
@@ -21,29 +23,39 @@ const requester = spawn("./mods/requester/requester.sh")
 const users = require(__dirname + "/../config/auth.json")
 const dispatcherConfig = getDispatcherConfig(__dirname + "/../config/dispatcher.json")
 const locationConfig = getLocationConfig(__dirname + "/../config/location.json")
+const registryConfig = getRegistryConfig(__dirname + "/../config/registry.json")
 const whiteList = process.env.WHITELIST ? process.env.WHITELIST.split(',') : []
 const logger = getLogger({service: "base", filePath: __filename})
 
 logger.info("routr v2 // connect distribution")
 
 if (dispatcherConfig._tag === 'Right') {
-  MessageDispatcher(dispatcherConfig.right)
+  messageDispatcher(dispatcherConfig.right)
 } else {
   logger.error(dispatcherConfig.left)
+  process.exit(1)
 }
 
 if (locationConfig._tag === 'Right') {
-  LocationService(locationConfig.right)
+  locationService(locationConfig.right)
 } else {
   logger.error(locationConfig.left)
+  process.exit(1)
+}
+
+if (registryConfig._tag === 'Right') {
+  registryService(registryConfig.right)
+} else {
+  logger.error(registryConfig.left)
+  process.exit(1)
 }
 
 const resources: Resource[] = loadResources(__dirname + "/../mods/simpledata/etc/schemas",
   __dirname + "/../config/resources")
 
-SimpleDataService({bindAddr: "0.0.0.0:52901", resources})
-SimpleAuthMiddleware({bindAddr: "0.0.0.0:51903", users, whiteList})
-ConnectProcessor({
+simpleDataService({bindAddr: "0.0.0.0:52901", resources})
+simpleAuthMiddleware({bindAddr: "0.0.0.0:51903", users, whiteList})
+connectProcessor({
   bindAddr: "0.0.0.0:51904",
   locationAddr: "localhost:51902",
   apiAddr: "localhost:52901"
