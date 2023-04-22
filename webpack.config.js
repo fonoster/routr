12a85ab77ac4454037b181ccaa810b3b4a5210b9
Pedro Sanders <psanders@fonoster.com>
2023-04-22 08:24:26 -0400

    chore: reduced size of edgeport bundle

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,52 @@
-var path = require('path');
+/*
+ * Copyright (C) 2023 by Fonoster Inc (https://fonoster.com)
+ * http://github.com/fonoster
+ *
+ * This file is part of Routr.
+ *
+ * Licensed under the MIT License (the "License");
+ * you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *    https://opensource.org/licenses/MIT
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+const path = require("path");
+const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = {
+  mode: "production",
   entry: {
-    edgeport: './dist/mods/edgeport/src/runner.js',
+    edgeport: "./dist/mods/edgeport/src/runner.js"
   },
   output: {
     filename: "[name].bundle.js",
-    path: path.resolve(__dirname, 'mods/edgeport/libs')
+    path: path.resolve(__dirname, "mods/edgeport/libs")
   },
   devtool: "source-map",
   resolve: {
-    extensions: ['.tsx', '.ts', '.js'],
+    extensions: [".tsx", ".ts", ".js"],
     fallback: {
-      "buffer": require.resolve("buffer/"),
-      "fs": false
+      buffer: require.resolve("buffer/"),
+      fs: false
     }
   },
   module: {
     rules: [
       {
         test: /\.tsx?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/,
-      },
-    ],
+        use: "ts-loader",
+        exclude: /node_modules/
+      }
+    ]
   },
-};
+  optimization: {
+    minimize: true,
+    minimizer: [new TerserPlugin()],
+  }
+}
