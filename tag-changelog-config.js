17faceb4a17fe125d320b4dd96392d07da0df80a
Pedro Sanders <psanders@fonoster.com>
2023-04-23 12:14:41 -0400

    ci: adds workflow to re-tag npm packages to @latest

--- tag-changelog-config.js
+++ tag-changelog-config.js
@@ -16,7 +16,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 module.exports = {
   types: [
     { types: ["feat", "feature"], label: "🎉 New Features" },
