aa57a1258f0e83351560ee744d882b91ebf88004
Pedro Sanders <psanders@fonoster.com>
2023-06-15 07:54:24 -0400

    fix: fixes not going back home

---
+++ assets/js/c4f5d8e4.9320406c.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkroutr_docs=self.webpackChunkroutr_docs||[]).push([[4195],{2841:(r,t,u)=>{u.r(t),u.d(t,{default:()=>o});var e=u(7294);function o(){return(0,e.useEffect)((()=>{window.location.href="https://routr.io"}),[]),null}}}]);
