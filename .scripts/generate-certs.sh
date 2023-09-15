364a631f11ce32573ef7b5f07482dd23726e9302
Pedro Sanders <psanders@fonoster.com>
2023-09-14 21:28:14 -0400

    chore: allows passing the ca and server subject for self-signed certs

--- .scripts/generate-certs.sh
+++ .scripts/generate-certs.sh
@@ -7,9 +7,9 @@ mkdir -p $basepath
 echo "Generating certificates in $basepath..."
 
 openssl genrsa -out $basepath/ca.key 2048
-openssl req -new -x509 -key $basepath/ca.key -out $basepath/ca.crt -subj "/CN=Default CA"
+openssl req -new -x509 -key $basepath/ca.key -out $basepath/ca.crt -subj "$CA_CERT_SUBJECT"
 openssl genrsa -out $basepath/server.key 2048
-openssl req -new -key $basepath/server.key -out $basepath/server.csr -subj "/CN=localhost"
+openssl req -new -key $basepath/server.key -out $basepath/server.csr -subj "$SERVER_CERT_SUBJECT"
 openssl x509 -req -in $basepath/server.csr -CA $basepath/ca.crt -CAkey $basepath/ca.key -CAcreateserial -out $basepath/server.crt
 
 rm $basepath/server.csr
